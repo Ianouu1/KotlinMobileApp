2f4ca667accf5f26537566b42ee1abc6015598c5
Ianouu1 <courrierdsf@gmail.com>
2024-05-17 11:48:12 +0200

    Calcul de surface pour : Aire d'un disque Surface d'un disque Volume d'une boule

--- app/src/main/java/com/example/mykotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/mykotlinapp/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.mykotlinapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,33 +17,66 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mykotlinapp.ui.theme.MyKotlinAppTheme
+import kotlin.time.times
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyKotlinAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.activity_main)
+
+        val aire_disque = findViewById<Button>(R.id.button1)
+        val surface_disque = findViewById<Button>(R.id.button2)
+        val volume_boule = findViewById<Button>(R.id.button3)
+        val resultview = findViewById<TextView>(R.id.textView2)
+        val rayonfromview = findViewById<EditText>(R.id.editTextNumber)
+
+        aire_disque.setOnClickListener(View.OnClickListener {
+            val rayon = rayonfromview.text.toString().toDoubleOrNull()
+            if (rayon != null) {
+                val result = Math.PI * rayon * rayon
+                resultview.text = "Resultat : %.2f".format(result) + "cm2"
             }
-        }
+        })
+
+        surface_disque.setOnClickListener(View.OnClickListener {
+            val rayon = rayonfromview.text.toString().toDoubleOrNull()
+            if (rayon != null) {
+                val result = 2*Math.PI * rayon
+                resultview.text = "Resultat : %.2f".format(result) + "cm2"
+            }
+        })
+
+        volume_boule.setOnClickListener(View.OnClickListener {
+            val rayon = rayonfromview.text.toString().toDoubleOrNull()
+            if (rayon != null) {
+                val result = (4/3.0)*Math.PI * Math.pow(rayon, 3.0)
+                resultview.text = "Resultat : %.2f".format(result) + "cm2"
+            }
+        })
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyKotlinAppTheme {
-        Greeting("Android")
+    override fun onStart() {
+        super.onStart()
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i("resume","onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i("pause","onPause")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i("stop","onStop")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("restart","onRestart")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("destroy", "onDestroy")
     }
 }
